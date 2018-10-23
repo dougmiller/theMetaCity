3c2e21de158e36a8318efeb4603f028b4025037a
Douglas Miller <dougmiller@themetacity.com>
2018-10-23 22:56:36 +1000

    Model is much better rounded out now. Many to many for the tags is in place. Parent for articles. Enum for type.

--- tmc/models.py
+++ tmc/models.py
@@ -1,29 +1,30 @@
 from tmc import db
-import enum
-
-class ArticleType(enum.Enum):
-    BLOG = 'blog'
-    WORKSHOP = 'workshop'
-
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique=True)
-    url = db.Column(db.String, unique=True)
-    type = db.Column(db.Enum(ArticleType))
-    text = db.Column(db.String)
-    date = db.Column(db.DateTime)
-
-    def __repr__(self):
-        return '<Article: {}>'.format(self.title)
 
 class Tags(db.Model):
+    __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.String, unique=True)
+    tag = db.Column(db.String, nullable=False)
 
     def __repr__(self):
         return '<Tag: {}>'.format(self.tag)
 
-class ArticleTags(db.Model):
-    article = db.Column(db.Integer, primary_key=True)
-    tag = db.Column(db.Integer, primary_key=True)
 
+tags_joiner = db.Table('tags_joiner',
+                       db.Column('tag', db.Integer, db.ForeignKey('tags.id'), primary_key=True),
+                       db.Column('article', db.Integer, db.ForeignKey('articles.id'), primary_key=True))
+
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    url = db.Column(db.String, unique=True)
+    type = db.Column(db.Enum('blog', 'workshop', name='type'), nullable=False, default='blog', server_default='blog')
+    text = db.Column(db.String)
+    creation_date = db.Column(db.DateTime, server_default=db.func.now())
+    update_date = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    parent = db.relationship('Article', backref='Parent', lazy='dynamic')
+    tags = db.relationship('Tags', secondary=tags_joiner, backref='articles')
+
+    def __repr__(self):
+        return '<Article {}: {}>'.format(self.id, self.title)
