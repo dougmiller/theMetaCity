eaa210ac1afa7279c682342b087ee77310031460
Douglas Miller <dougmiller@themetacity.com>
2019-01-26 13:08:07 +1000

    Refined where workshop article and blog articles show up. Filled out the templates for the workshops

--- tmc/routes.py
+++ tmc/routes.py
@@ -11,21 +11,29 @@ def index():
 
 @tmc.route('/blog')
 def blog():
-    articles = Article.query.order_by(Article.creation_date.desc()).limit(10).all()
+    articles = Article.query.\
+        filter_by(type='blog')\
+        .order_by(Article.creation_date.desc())\
+        .limit(10)\
+        .all()
     return render_template('blog/index.html', articles=articles)
 
 
 @tmc.route('/blog/<string:url>')
 def blog_with_title(url):
-    article = Article.query.filter_by(url=url)\
-        .filter_by(type='blog') \
+    article = Article.query.\
+        filter_by(url=url)\
+        .filter_by(type='blog')\
         .first_or_404()
     return render_template('blog/article.html', article=article)
 
 
 @tmc.route('/blog/<int:year>')
 def blog_with_year(year):
-    articles = Article.query.filter(db.func.extract('year', Article.creation_date) == year).all()
+    articles = Article.query\
+        .filter(db.func.extract('year', Article.creation_date) == year)\
+        .filter_by(type='blog')\
+        .all()
     return render_template('blog/index.html', articles=articles)
 
 
@@ -33,6 +41,7 @@ def blog_with_year(year):
 def blog_with_year_and_title(year, url):
     article = Article.query\
         .filter(db.func.extract('year', Article.creation_date) == year)\
+        .filter_by(type='blog')\
         .filter_by(url=url).first_or_404()
     return render_template('blog/article.html', article=article)
 
@@ -41,7 +50,8 @@ def blog_with_year_and_title(year, url):
 def blog_with_year_and_month(year, month):
     articles = Article.query\
         .filter(db.func.extract('year', Article.creation_date) == year)\
-        .filter(db.func.extract('month', Article.creation_date) == month).all()
+        .filter(db.func.extract('month', Article.creation_date) == month).all()\
+        .filter_by(type='blog')
     return render_template('blog/index.html', articles=articles)
 
 
@@ -50,6 +60,7 @@ def blog_with_year_and_month_and_title(year, month, url):
     article = Article.query\
         .filter(db.func.extract('year', Article.creation_date) == year)\
         .filter(db.func.extract('month', Article.creation_date) == month)\
+        .filter_by(type='blog')\
         .filter_by(url=url).first_or_404()
     return render_template('blog/article.html', article=article)
 
