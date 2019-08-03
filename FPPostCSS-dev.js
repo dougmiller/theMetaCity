7b1df937021484473ed62640fd496ec57c91adec
Douglas Miller <dougmiller@themetacity.com>
2019-08-03 11:05:14 +1000

    NPM cleaned up a little. Moe consistant now.

--- FPPostCSS-dev.js
+++ FPPostCSS-dev.js
@@ -4,7 +4,7 @@ let FPPostCSS = require('postcss');
 let fs = require('fs');
 let postcss_import = require('postcss-import');
 let postcss_custom_media = require('postcss-custom-media');
-let postcss_css_letiables = require('postcss-css-letiables');
+let postcss_css_variables = require('postcss-css-variables');
 let postcss_autoprefixer = require('autoprefixer');
 let postcss_reporter = require('postcss-reporter');
 let postcss_nested = require('postcss-nested');
@@ -20,7 +20,7 @@ let css = fs.readFileSync("pcss/front-page.pcss", "utf8");
 FPPostCSS([
     postcss_import,
     postcss_custom_media,
-    postcss_css_letiables,
+    postcss_css_variables,
     postcss_nested,
     postcss_autoprefixer,
     postcss_reporter
