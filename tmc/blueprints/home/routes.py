93c6f383b467fb818bac4f8d850d7dfc46357027
Douglas Miller <doug@knd.com.au>
2025-04-15 13:02:49 +1000

    Nice imports aon home

--- tmc/blueprints/home/routes.py
+++ tmc/blueprints/home/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template, make_response
-from tmc import cache
-from tmc.extensions import db
+from tmc.extensions import db, cache
 from . import home
 from tmc.models.blog import Article, Blog, Workshop, Tag
 
@@ -13,14 +12,6 @@ def index():
           .limit(3)
     ).scalars().all()
     return render_template('home/index.jinja2', articles=articles)
-
-
-@home.route('/abc/')
-def abc():
-    articles = db.session.execute(
-        db.select(Article).order_by(Article.created_at.desc())
-    ).scalars()
-    return render_template('home/123.jinja2', articles=articles)
 
 
 @home.route('/about/')
