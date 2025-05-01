04be68e8909dde005a39b18c7e25ebd6e6d5f9ec
Douglas Miller <doug@knd.com.au>
2025-05-01 15:44:44 +1000

    Quick lint of the image models file

--- tmc/models/media/images.py
+++ tmc/models/media/images.py
@@ -1,36 +1,41 @@
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from tmc.models.media import MediaItem, MediaType
 from tmc.models.extensions import UUIDModel
+
 
 class Gallery(MediaItem):
 	__tablename__ = "gallery"
 	__table_args__ = {"schema": "media"}
 	__bind_key__ = "media"
 	__mapper_args__ = {
-		'polymorphic_identity': MediaType.gallery
+		"polymorphic_identity": MediaType.gallery
 	}
-	
-	id: Mapped[int] = mapped_column(ForeignKey("media.media_item.id"), primary_key=True)
+
+	id: Mapped[int] = mapped_column(
+		ForeignKey("media.media_item.id"),
+		primary_key=True
+	)
 
 	items: Mapped[list["Image"]] = relationship(
 		back_populates="gallery",
-		cascade="all, delete-orphan",
+		passive_deletes=True,
+		cascade="save-update, merge",
 		lazy="select"
 	)
 
 
 class Image(UUIDModel):
-	'''   
-	This design supports the concept of orphaned entries by allowing `gallery_id` to remain nullable or unset, 
-	depending on the broader schema and application logic. An image can exist independently of a gallery if needed, 
-	while still enabling tallying via `gallery_id` when present. This makes it possible to treat a lone image as a 
-	gallery of one, ensuring flexibility in the way media items are organized and displayed.
-	'''
+	"""
+	Represents an image that can be added to a Gallery.
+
+	NB: They do not have to be, but the design of the site assumes it otherwise.
+	"""
 	__tablename__ = "image"
 	__table_args__ = {"schema": "media"}
 	__bind_key__ = "media"
-	
+
 	gallery_id: Mapped[int | None] = mapped_column(
 		ForeignKey(
 			"media.gallery.id",
@@ -39,4 +44,6 @@ class Image(UUIDModel):
 		),
 		nullable=True
 	)
-	gallery: Mapped[Gallery | None] = relationship(back_populates="items")
+	gallery: Mapped[Gallery | None] = relationship(
+		back_populates="items"
+	)
