1b6599c24b0c83b3176525c5ef9ded553624106b
Douglas Miller <doug@knd.com.au>
2025-05-06 14:15:16 +1000

    Starting some lining and formatting controls with pyproject mk2

--- tmc/models/media/audio.py
+++ tmc/models/media/audio.py
@@ -5,11 +5,11 @@ from tmc.models.media import MediaItem, MediaType
 
 
 class Audio(MediaItem):
-	__tablename__ = "audio"
-	__table_args__ = {"schema": "media"}
-	__bind_key__ = "media"
-	__mapper_args__ = {
-		'polymorphic_identity': MediaType.audio
-	}
+    __tablename__ = "audio"
+    __table_args__ = {"schema": "media"}
+    __bind_key__ = "media"
+    __mapper_args__ = {
+        "polymorphic_identity": MediaType.audio,
+    }
 
-	id: Mapped[int] = mapped_column(ForeignKey("media.media_item.id"), primary_key=True)
+    id: Mapped[int] = mapped_column(ForeignKey("media.media_item.id"), primary_key=True)
