5fc8cc0ef65c1900f690d9a54a382a758a0d9ee2
Douglas Miller <doug@knd.com.au>
2025-05-22 13:34:10 +1000

    markdown filter now

--- tmc/extensions/jinja_filters/__init__.py
+++ tmc/extensions/jinja_filters/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from markupsafe import Markup
 
 
-def tmc_markdown(text):
+def tmc_markdown(raw_markdown):
 	"""Render Markdown safely as HTML."""
 	from tmc.extensions import md
-	return Markup(md.convert(text))
+	return Markup(md.convert(raw_markdown))
 
 def register_filters(app: Flask):
 	"""Register custom filters with the Flask app."""
-	app.jinja_env.filters["tmc_markdown"] = tmc_markdown
+	app.jinja_env.filters["markdown"] = tmc_markdown
