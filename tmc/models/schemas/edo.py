e0b4323a53de932f8bcaf60cce52bae6d33cc0f5
Douglas Miller <doug@knd.com.au>
2025-05-29 13:43:52 +1000

    EDO api handles dates from Arrow types with an ISO resposne

--- tmc/models/schemas/edo.py
+++ tmc/models/schemas/edo.py
@@ -1,16 +1,23 @@
 from tmc.models.edo import EDO
 from tmc.extensions.marshmallow import ma
 from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from marshmallow import fields
 
 __all__ = ('EDOSchema')
 
 
 class EDOSchema(ma.SQLAlchemyAutoSchema):
-	class Meta:
-		model = EDO
-		load_instance = True
-
-	id = auto_field()
-	content = auto_field()
-	created_at = auto_field()
-	updated_at = auto_field()
+    class Meta:
+        model = EDO
+        load_instance = True
+    
+    id = auto_field()
+    content = auto_field()
+    created_at = fields.Method("get_created_at")
+    updated_at = fields.Method("get_modified_at")
+    
+    def get_created_at(self, obj):
+        return obj.created_at.isoformat() if obj.created_at else None
+    
+    def get_modified_at(self, obj):
+        return obj.created_at.isoformat() if obj.created_at else None
