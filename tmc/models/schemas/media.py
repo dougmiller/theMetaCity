ed76a7f54485eb4230bdf9913ba1bdfef45dbe03
Douglas Miller <doug@knd.com.au>
2025-05-30 12:11:34 +1000

    Schemas getting check too

--- tmc/models/schemas/media.py
+++ tmc/models/schemas/media.py
@@ -1,41 +1,40 @@
-from tmc.models.media import MediaItem, Licence, Postcard
-from tmc.extensions.marshmallow import ma
-from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from marshmallow_sqlalchemy import auto_field
 from marshmallow_sqlalchemy.fields import Nested
 
-__all__ = ('MediaItemSchema')
+from tmc.extensions.marshmallow import ma
+from tmc.models.media import Licence, MediaItem, Postcard
+
+__all__ = "MediaItemSchema"
 
 
 class LicenceSchema(ma.SQLAlchemySchema):
-	class Meta:
-		model = Licence
-		load_instance = True
+    class Meta:
+        model = Licence
+        load_instance = True
 
-	id = auto_field()
-	name = auto_field()
-	text = auto_field()
-	url = auto_field()
-	image = auto_field()
+    id = auto_field()
+    name = auto_field()
+    text = auto_field()
+    url = auto_field()
+    image = auto_field()
 
 
 class PostcardSchema(ma.SQLAlchemySchema):
-	class Meta:
-		model = Postcard
-		load_instance = True
+    class Meta:
+        model = Postcard
+        load_instance = True
 
-	id = auto_field()
-	title = auto_field()
-	url = auto_field()
+    id = auto_field()
+    title = auto_field()
+    url = auto_field()
 
 
 class MediaItemSchema(ma.SQLAlchemySchema):
-	class Meta:
-		model = MediaItem
-		load_instance = True
+    class Meta:
+        model = MediaItem
+        load_instance = True
 
-	id = auto_field()
-	title = auto_field()
-	postcard = Nested(PostcardSchema)
-	licence = Nested(LicenceSchema)
-
-
+    id = auto_field()
+    title = auto_field()
+    postcard = Nested(PostcardSchema)
+    licence = Nested(LicenceSchema)
