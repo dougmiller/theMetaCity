2ac89ce7b6e9e0395f5d373a8b42334b95361141
Douglas Miller <doug@knd.com.au>
2025-06-17 10:36:13 +1000

    Gallery is in the API now

--- tmc/blueprints/api/media/v1/__init__.py
+++ tmc/blueprints/api/media/v1/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from .audio import audio
 from .video import video
 from .image import image
+from .gallery import gallery
 
 
 v1 = Blueprint(
@@ -13,5 +14,6 @@ v1 = Blueprint(
 v1.register_blueprint(audio, url_prefix="/audio")
 v1.register_blueprint(video, url_prefix="/video")
 v1.register_blueprint(image, url_prefix="/image")
+v1.register_blueprint(gallery, url_prefix="/gallery")
 
 from . import routes
